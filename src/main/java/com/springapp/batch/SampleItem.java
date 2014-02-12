322a3ed1bc8debbf5dc2d40d7e8a2420b01fb117
Klaus Torhaugen <klaus.torhaugen@gmail.com>
2014-02-12 08:37:08 +0100

    Test for å lære springBatch

---
+++ src/main/java/com/springapp/batch/SampleItem.java
@@ -0,0 +1,17 @@
+package com.springapp.batch;
+
+public class SampleItem {
+    private String name;
+
+    public SampleItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
